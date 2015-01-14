09fe5f82e3651a64e8d9ff3938bf12bf5b9fe44c
jipson7 <caleb.phillips@uoit.net>
2015-01-13 19:21:31 -0500

    Updated with search ability, currently searches for open room on 3rd floor based on specified time and date variables

--- booker.py
+++ booker.py
@@ -2,12 +2,44 @@ import mechanize
 import re
 import urllib2
 import time
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 
-month = time.strftime("%B")
-day = time.strftime("%d")
 
-dateString = month + " " + day
+def makeList(table):
+    result = []
+    allrows = table.findAll('tr')
+    for row in allrows:
+        result.append([])
+        allcols = row.findAll('td')
+        for col in allcols:
+            thestrings = [unicode(s) for s in col.findAll(text=True)]
+            thetext = ''.join(thestrings)
+            result[-1].append(thetext)
+    return result
+
+def getDateString():
+    
+    month = time.strftime("%B")
+    
+    day = time.strftime("%d")
+   
+    dateString = month + " " + day
+
+    return dateString
+
+def innerHTML(element):
+    return element.decode_contents(formatter="html")
+
+def searchThirdFloor(time):
+    for ii in thirdFloorRooms:
+        domTitle = "Time: " + time + ". Room no.: " + ii
+        matchingLinks = tableLinkSoup.find_all(attrs={"title" : domTitle})
+        for testLink in matchingLinks:
+            imgString = innerHTML(testLink)
+            if "images/open.gif" in imgString:
+                print imgString
+
+dateString = getDateString()
 
 try :
     web_page = urllib2.urlopen("https://rooms.library.dc-uoit.ca/uoit_studyrooms/calendar.aspx").read()
@@ -30,12 +62,8 @@ secondParam = dateUrl[-6:-2]
 
 br = mechanize.Browser()
 
-print br
-
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 response = br.open("https://rooms.library.dc-uoit.ca/uoit_studyrooms/calendar.aspx")
-
-html = response.read()
 
 br.select_form(nr=0)
 br.set_all_readonly(False)
@@ -43,10 +71,15 @@ br["__EVENTTARGET"] = firstParam
 br["__EVENTARGUMENT"] = secondParam
 response = br.submit()
 
-#mnext = re.search("""<a href="javascript:__doPostBack('(.*?)','(.*?)')" style="color:Black" title="January 13">13""", html)
 
 tableHtml = response.read()
 
-tableUrl = response.geturl()
+tableLinkSoup = BeautifulSoup(tableHtml, parse_only=SoupStrainer('a'))
 
-print tableHtml
+userSelectedTime = "8:00 PM"
+
+thirdFloorRooms = ['LIB303', 'LIB304', 'LIB305', 'LIB306', 'LIB307', 'LIB309', 'LIB310']
+
+secondFloorRooms = ['LIB202A', 'LIB202B', 'LIB202C']
+
+searchThirdFloor(userSelectedTime)
