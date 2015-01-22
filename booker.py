8b4d3033b14ed3444149960dcf9d96a1f0fe83c5
jipson7 <caleb.phillips@uoit.net>
2015-01-22 12:16:09 -0500

    Added functionality for user to select day and time. It also check multiple library rooms. To find a room with a full 2 hour of open slots at the specified time. Next step is refactorizing the fill in form function, and adding multiple users

--- booker.py
+++ booker.py
@@ -1,35 +1,154 @@
 import time
+import datetime
+import sys
+import os
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException 
 
-STUDENT_INFORMATION = ["XXXXXXXXX", "XXXXXXXXXX"]
+STUDENT_INFORMATION = ["100518555", "Siahjack1"]
 
-def getDateString():
+TIME_SELECTION = ["8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM",
+        "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM",
+        "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM",
+        "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM",
+        "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM"]
 
-    month = time.strftime("%B")
+ROOM_SELECTION = ["LIB310", "LIB309", "LIB307", "LIB306", "LIB305", "LIB304", 
+        "LIB303", "LIB202C", "LIB202B", "LIB202A"]
+
+datePickingURL = "https://rooms.library.dc-uoit.ca/uoit_studyrooms/calendar.aspx"
+
+userSelectedTime = ""
+
+userSelectedDate = ""
+
+scriptSelectedRoom = ""
+
+def askUserQuestions():
+
+    global userSelectedDate
+
+    global userSelectedTime
     
-    day = time.strftime("%d")
-           
-    dateString = month + " " + "21"
+    print "Are you booking for (1) Today or (2) Tomorrow"
+
+    while (True):
+
+        userDay = raw_input("(Select 1 or 2): ")
+
+        if (userDay == "1"):
+
+            userSelectedDate = getDateString(userDay)
+
+            break
+
+        elif (userDay == "2"):
+            
+            userSelectedDate = getDateString(userDay)
+
+            break
+
+    print "\nWhat time would you like to book for?"
+
+    while(True):
+
+        userSelectedTime = raw_input("(Ex. 10:00 AM or 8:00 PM): ")
+
+        if userSelectedTime in TIME_SELECTION:
+
+            break
+
+        else:
+
+            print "Invalid entry, try again (Ex. 5:00 PM)"
+    
+
+def getDateString(stringDay):
+
+    intDay = int(stringDay)
+
+    dateString = "";
+
+    if (intDay == 1):
+        
+        month = time.strftime("%B")
+        
+        day = time.strftime("%d")
+
+
+        dateString = month + " " + day
+
+    elif (intDay == 2):
+
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        tm_stme = datetime.time(0, 0, 0)
+        tm_stdate = datetime.datetime.combine(tomorrow, tm_stme)
+
+        day = tm_stdate.strftime('%d')
+
+        month = tm_stdate.strftime('%B')
+
+        dateString = month + " " + day
+
+    else:
+        print "Error in getting Date String"
+        return
 
     return dateString
 
-def requestTimeFromUser():
+def getOpenRoomTag():
 
-    return "Time: 8:00 PM. Room no.: LIB303"
+    #return ("Time: " + userSelectedTime + ". Room no.: LIB303")
 
-def checkConsecutiveFreeTime():
-    return true
+   global scriptSelectedRoom
 
-def checkIfTimeSlotIsFree():
-    return true
+   for room in ROOM_SELECTION:
+       
+       if checkConsecutiveFreeTime(userSelectedTime, room):
+
+           scriptSelectedRoom = room
+
+           return buildTimeRoomTag(userSelectedTime, room)
+
+   print "No available rooms at the specified time"
+
+   sys.exit()
+
+def checkConsecutiveFreeTime(userTime, room):
+
+    timeLocation = TIME_SELECTION.index(userTime)
+        
+    for ii in range(4):
+
+        if not checkIfTimeSlotIsFree(TIME_SELECTION[ii], room):
+
+            return False
+
+    return True
+
+def checkIfTimeSlotIsFree(userTime, room):
+  
+
+   try:
+       driver.find_element_by_css_selector(
+               "img[title='" + buildTimeRoomTag(userTime, room) + "']")
+   
+   except NoSuchElementException:
+       return False
+
+   return True
+
+def buildTimeRoomTag(userTime, room):
+    
+    return ("Time: " + userSelectedTime + ". Room no.: " + room)
 
 def clickLinkByTitle(title):
     driver.find_element_by_css_selector("a[title='" + title  + "']").click() 
 
-def fillInForm(studentNo, password):
+def fillInFormInitial(studentNo, password):
    studentElement =  driver.find_element_by_css_selector(
            "input[name='ctl00$ContentPlaceHolder1$TextBoxStudentID']")
 
@@ -65,9 +184,26 @@ def fillInForm(studentNo, password):
 
    submitButton.click()
 
+    
+
+   try:
+
+
+      element = WebDriverWait(driver, 10).until(
+
+          EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_LabelMessage"))
+      )
+
+   except:
+
+      print "Error filling out form"
+
+   
+
 ###########################################################################
 
-datePickingURL = "https://rooms.library.dc-uoit.ca/uoit_studyrooms/calendar.aspx"
+askUserQuestions()
+
 
 try:
 
@@ -91,13 +227,11 @@ except:
 
     print "Error loading inital page"
 
-dateSelection = getDateString()
-
 #time.sleep(1)
 
-clickLinkByTitle(dateSelection)
+clickLinkByTitle(userSelectedDate)
 
-timeSelection = requestTimeFromUser()
+timeSelection = getOpenRoomTag()
 
 #time.sleep(1)
 
@@ -114,4 +248,6 @@ except:
 
     print "Error following first link/loading second page"
 
-fillInForm(STUDENT_INFORMATION[0], STUDENT_INFORMATION[1])
+fillInFormInitial(STUDENT_INFORMATION[0], STUDENT_INFORMATION[1])
+
+print ("Successfully booked " + scriptSelectedRoom + " at " + userSelectedTime)
